503937fc70b4a1315a83e1ab95819931208e8695
shasthri kawya <99796618+IT21165016@users.noreply.github.com>
2022-02-26 12:28:06 +0000

    tutorial 03-ex1:

--- exercise01.cpp
+++ exercise01.cpp
@@ -17,9 +17,11 @@ int main()
 {
   float marks[]={78.4,90.6,45.9,72.2,54.4};
   char names[][20]=     {"Ajith","Wimal","Kanthi","Suranji","Kushmitha"};
-  cout<<"No"<<"\t"<<"Name"<<"\t"<<"\t"<<"Marks"<<endl;
+  cout<<0<<"\t"<<"\t"<<"\t"<<1<<"\t"<<"\t"<<"\t"<<2<<"\t"<<"\t"<<"\t"<<3<<endl;
+  cout<<0<<1<<2<<3<<4<<5<<6<<7<<8<<9<<0<<1<<2<<3<<4<<5<<6<<7<<8<<9<<0<<1<<2<<3<<4<<5<<6<<7<<8<<9<<0<<0<<1<<2<<3<<4<<5<<6<<7<<8<<9<<0<<endl;
+  cout<<setw(5)<<"No"<<setw(15)<<"Name"<<setw(10)<<"Marks"<<endl;
   for(int i=0;i<5;i++){
-      cout<<i+1<<"\t"<<names[i]<<"\t"<<"\t"<<marks[i]<<setiosflags(ios::fixed)<<setprecision(2)<<endl;
+      cout<<setw(5)<<i+1<<setw(15)<<names[i]<<setw(10)<<marks[i]<<setiosflags(ios::fixed)<<setprecision(2)<<endl;
     }
 return 0;
   
