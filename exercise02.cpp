28094fdac9720136ea204617f4a868babc25e829
shasthri kawya <99796618+IT21165016@users.noreply.github.com>
2022-02-27 14:17:29 +0000

    tutorial 03-ex 02

--- exercise02.cpp
+++ exercise02.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 using namespace std;
 
 int volume(int height, int width, int length);
@@ -28,6 +28,40 @@ int main() {
     cout << "Volume of Box is " << totalVolume << endl;
     
     return 0;
-}
+}*/
 
 // Implement the Volume() function here
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int volume(int height,int width,int length);
+struct box
+{
+  int height;
+  int width;
+  int length;
+};
+int main()
+{
+  struct box box1,box2;
+  int totalVolume;
+  cout<<"Enter Box 1 Height:";
+  cin>>box1.height;
+  cout<<"Enter Box 1 Width:";
+  cin>>box1.width;
+  cout<<"Enter Box 1 Length:";
+  cin>>box1.length;
+  
+  cout<<"Enter Box 2 Height:";
+  cin>>box2.height;
+  cout<<"Enter Box 2 Width:";
+  cin>>box2.width;
+  cout<<"Enter Box 2 Length:";
+  cin>>box2.length;
+
+  totalVolume=volume(box1.height,box1.width,box1.length)+
+        volume(box2.height,box2.width,box2.length);
+
+  cout<<"Volume of box is "<<totalVolume<<endl;
+  return 0;
+}
